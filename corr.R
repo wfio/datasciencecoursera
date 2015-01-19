33f8a3d5b9a8d53b36fe6fc9428777ecd016a17a
Zach <zach@wayfaring.io>
2015-01-18 23:06:23 -0800

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,17 +1,18 @@
 corr <- function(directory, threshold) {
     
     directory <- ("/media//solaris//HotSwap//coursera//datasciencecoursera//specdata")
-    id <- list.files(directory, full.names = TRUE) #call this file_names in the end
+    id <- list.files(directory, full.names = TRUE)
     filenames <- data.frame(id)
     threshold <- vector("numeric")
-    cr <- vector("numeric", length = 1)
+    cr <- vector("numeric")
     
     for(i in 1:length(id)){
-        filenames <- paste(threshold, id[i], sep = "") #call filenames 'id' in the end?
+        filenames <- sprintf("%s/%i", threshold, id[i])
         cr[i] <- sum(complete.cases(read.csv(filenames)))
     
         }#end-for
     
     print("I made it to the end")
+    data.frame(id, cr)
 }
 
