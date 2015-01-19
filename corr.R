fe7869fc4a2cadfa190c2e50a0a1fc4ce6d78d86
Zach <zach@wayfaring.io>
2015-01-18 22:40:40 -0800

    Fixed logic now flows to end of script

--- corr.R
+++ corr.R
@@ -1,17 +1,17 @@
 corr <- function(directory, threshold) {
     
-    directory <- c("/media//solaris//HotSwap//coursera//datasciencecoursera//specdata")
+    directory <- ("/media//solaris//HotSwap//coursera//datasciencecoursera//specdata")
     id <- list.files(directory, full.names = TRUE) #call this file_names in the end
     filenames <- data.frame(id)
-    threshold <- vector("numeric", length = 1)
+    threshold <- vector("numeric")
     cr <- vector("numeric", length = 1)
     
     for(i in 1:length(id)){
-        filenames <- sprintf("%s/%s", threshold, id[i]) #call filenames 'id' in the end?
-        nobs[i] <- sum(complete.cases(read.csv(filenames)))
+        filenames <- paste(threshold, id[i], sep = "") #call filenames 'id' in the end?
+        cr[i] <- sum(complete.cases(read.csv(filenames)))
     
         }#end-for
-    print("I made it to the end")
     
+    print("I made it to the end")
 }
 
