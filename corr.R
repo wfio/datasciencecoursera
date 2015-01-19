37a18951479f5a63bf6fd2a36290f08035386d8e
Zach <zach@wayfaring.io>
2015-01-18 22:00:17 -0800

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,20 +1,13 @@
-source("corr.R")
-
 corr <- function(directory, threshold) {
     
-    ids <- list.files("specdata/", full.names = TRUE)
-    
-    
     directory <- c("/media//solaris//HotSwap//coursera//datasciencecoursera//specdata")
+    id <- list.files(directory, full.names = TRUE)
     threshold <- vector("numeric", length = 1)
     cr <- vector("numeric", length = 1)
     
     for(i in 1:length(id)){
         filenames <- sprintf("%s/%03d.csv", directory, id[i])
         nobs[i] <- sum(complete.cases(read.csv(filenames)))
-    }
-        
-    }
     
-    
-}#end-func
+        }#end-for
+}
