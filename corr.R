1b560157d0f1c1ed9dbdbee7ddb7dac8b86a249d
Zach <zach@wayfaring.io>
2015-01-18 21:50:57 -0800

    Different for loop

--- corr.R
+++ corr.R
@@ -2,19 +2,19 @@ source("corr.R")
 
 corr <- function(directory, threshold) {
     
-    threshold <- list.files("specdata/", full.names = TRUE)
+    ids <- list.files("specdata/", full.names = TRUE)
     
     
-   directory <- c("/media//solaris//HotSwap//coursera//datasciencecoursera//specdata")
-   threshold <- vector("numeric", length = 1)
-   cr <- vector("numeric", length = 1)
-   
-   for(i in 1:seq_along(threshold)) {
-   
-       threshold <- complete.cases(directory(directory$sulfate & directory$nitrate > threshold[i]))
-       
-   }
+    directory <- c("/media//solaris//HotSwap//coursera//datasciencecoursera//specdata")
+    threshold <- vector("numeric", length = 1)
+    cr <- vector("numeric", length = 1)
     
-cr <- corr(directory$sulfate, directory$nitrate)
+    for(i in 1:length(id)){
+        filenames <- sprintf("%s/%03d.csv", directory, id[i])
+        nobs[i] <- sum(complete.cases(read.csv(filenames)))
+    }
+        
+    }
+    
     
 }#end-func
