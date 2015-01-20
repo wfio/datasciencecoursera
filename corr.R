3ff663eab4d564f564d49ffbad9139377a6a886a
Zach <zach@wayfaring.io>
2015-01-19 16:03:47 -0800

    Update corr.R

--- corr.R
+++ corr.R
@@ -2,26 +2,20 @@ corr <- function(directory, threshold) {
     
     directory <- ("/media//solaris//HotSwap//coursera//datasciencecoursera//specdata")
     id <- list.files(directory, full.names = TRUE)
-    filenames <- data.frame(id)
-    threshold <- vector("numeric")
+    filenames <- data.frame(id)    
     cr <- vector("numeric")
     output <- vector("numeric")
     
     for(i in 1:332) {
     
-        filenames <- paste(threshold, id[i], sep = "")
-        cr[i] <- sum(complete.cases(read.csv(filenames)))
+        #filenames <- paste(threshold, id[i], sep = "")
+        cr[i] <- sum(complete.cases(read.csv(id[i])))
         results <- numeric(cr[i])
         
-        if(cr <= threshold) {
-            next
-        }
+       
         
         }#end-for
     
-    print("I made it to the end")
-    cr
-    #data.frame(cr, threshold)
-    ##output <- cor(id, threshold)
+    print("This is a test -- I made it to the end")
+    data.frame(cr, threshold)
 }
-
